9f8860b7996af71565830a6b924c15950328f272
Jason Wong <jason.wong@twotalltotems.com>
2017-01-15 16:01:18 -0800

    Detect mouse click

--- MusicPlayer/game.py
+++ MusicPlayer/game.py
@@ -7,15 +7,30 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption('My Game')
 clock = pygame.time.Clock()
 
+cupcakeImg = pygame.image.load('cupcake.png')
+cupcakeImg = pygame.transform.scale(cupcakeImg, (200, 200))
+
+def drawImage(image, x, y):
+	cupcakeDraw = screen.blit(image, (x,y))
+	return cupcakeDraw
+
+
 done = False
 while done == False:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
-		print(event)
+		# print(event)
 
+	
 	screen.fill((255, 255, 255))
+	cupcakeDraw = drawImage(cupcakeImg, 50, 50)
 
+	pos = pygame.mouse.get_pos()
+	(pressed1,pressed2,pressed3) = pygame.mouse.get_pressed()
+
+	if cupcakeDraw.collidepoint(pos)& pressed1==1:
+		print("You have opened a chest!")
 	pygame.display.update()
 	clock.tick(60)
 
