7bb08018e4f0645e7d4b217940424fb89d954e57
Jason Wong <jason_19960903@hotmail.com>
2017-03-19 17:00:22 -0700

    Now quits the app when it hits boundary

---
+++ Snake/food.py
@@ -0,0 +1,7 @@
+class Food:
+	COLOR = (189, 183, 107)
+	WIDTH = 30
+	HEIGHT = 30
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
--- Snake/game.py
+++ Snake/game.py
@@ -12,10 +12,12 @@ class Direction(Enum):
 
 #importing pygame and the mixer
 import pygame
+from random import randint
+from food import Food
 pygame.init()
 
 #setting screen size, border, and caption
-size = [ 640, 480 ]
+size = [ 660, 480 ]
 screen = pygame.display.set_mode( size )
 pygame.display.set_caption ( 'Justin and Jasons Snake game' )
 clock = pygame.time.Clock ()
@@ -28,10 +30,10 @@ SNAKE_BODY_COLOR = (255, 0, 0)
 SNAKE_BODY_WIDTH = 30
 SNAKE_BODY_HEIGHT = 30
 
-SNAKE_SPEED = 15
+SNAKE_SPEED = 30
 SNAKE_X = 0
 SNAKE_Y = 0
-SNAKE_INITIAL_DIRECTION = Direction.DOWN
+SNAKE_DIRECTION = Direction.DOWN
 
 def drawSnakePart(x, y, isHead):
 	if isHead :
@@ -39,25 +41,47 @@ def drawSnakePart(x, y, isHead):
 	else:
 		pygame.draw.rect(screen, SNAKE_BODY_COLOR, (x, y, SNAKE_BODY_WIDTH, SNAKE_BODY_HEIGHT))
 
+def generateFoodLocation():
+	x = randint(0, size[0]/30 - 1) * 30
+	y = randint(0, size[1]/30 - 1) * 30
+	return (x, y)
+
+def drawFood(x, y):
+	food = Food(x, y)
+	pygame.draw.rect(screen, Food.COLOR, (food.x, food.y, Food.WIDTH, Food.HEIGHT))
 
 #all the fps and quit info is here.
 done = False
 while done == False:
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				SNAKE_DIRECTION = Direction.LEFT
+			elif event.key == pygame.K_UP:
+				SNAKE_DIRECTION = Direction.UP
+			elif event.key == pygame.K_RIGHT:
+				SNAKE_DIRECTION = Direction.RIGHT
+			elif event.key == pygame.K_DOWN:
+				SNAKE_DIRECTION = Direction.DOWN
+		elif event.type == pygame.QUIT:
 			done = True
 	
 	screen.fill((255, 255, 255))
 	drawSnakePart(SNAKE_X, SNAKE_Y, True)
-	if SNAKE_INITIAL_DIRECTION == Direction.UP:
+	(foodX,foodY) = generateFoodLocation()
+	drawFood(foodX, foodY)
+	if SNAKE_DIRECTION == Direction.UP:
 		SNAKE_Y = SNAKE_Y - SNAKE_SPEED
-	elif SNAKE_INITIAL_DIRECTION == Direction.LEFT:
+	elif SNAKE_DIRECTION == Direction.LEFT:
 		SNAKE_X = SNAKE_X - SNAKE_SPEED
-	elif SNAKE_INITIAL_DIRECTION == Direction.DOWN:
+	elif SNAKE_DIRECTION == Direction.DOWN:
 		SNAKE_Y = SNAKE_Y + SNAKE_SPEED
 	else:
 		SNAKE_X = SNAKE_X + SNAKE_SPEED
+	
 	pygame.display.update()
-	clock.tick(8)
+	if SNAKE_X < 0 or SNAKE_X > size[0]-SNAKE_BODY_WIDTH or SNAKE_Y < 0 or SNAKE_Y > size[1]-SNAKE_BODY_HEIGHT:
+		done = True
+	clock.tick(5)
 
 pygame.quit()
